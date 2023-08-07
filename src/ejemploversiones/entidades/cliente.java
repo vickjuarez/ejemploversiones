831e721d7f3604894664ca6b624878a541d47e98
vickjuarez <victoriajuarez287@gmail.com>
2023-08-07 20:25:00 -0300

    agrega clase producto

--- src/ejemploversiones/entidades/cliente.java
+++ src/ejemploversiones/entidades/cliente.java
@@ -5,10 +5,30 @@
  */
 package ejemploversiones.entidades;
 
-/**
- *
- * @author Diego Robles
- */
 public class cliente {
+    private int codigo;
+    private String descripcion;
+
+    public cliente(int codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     
 }
